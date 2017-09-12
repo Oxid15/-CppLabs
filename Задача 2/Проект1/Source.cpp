9068939cf16e8424889eb90eb4f32db7698d1ba0
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2017-09-12 22:55:52 +0500

    3

--- Задача 2/Проект1/Source.cpp
+++ Задача 2/Проект1/Source.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 float a1, b1, c1, a2, b2, c2;
 int main()
 {
-	std::cout << "Write a1 \n" ;
-	std::cin >> a1;
-	std::cout << "Write a2 \n" ;
+	std::cout << "Write a1 \n"; std::cin >> a1;
+	std::cout << "Write b2 \n"; std::cin >> b2;
+	std::cout << "Write c1 \n"; std::cin >> c1;
+	std::cout << "Write a2 \n"; std::cin >> a2;
+	std::cout << "Write b2 \n"; std::cin >> b2;
+	std::cout << "Write c2 \n"; std::cin >> c2;
+
+	if ((abs(a1*b2 - a2*b1))>=0.001)
+	
+	{
+		a1*x + b1*y + c2 = 0;
+		a2*x + b2*y + c2 = 0;
+	}
 }
