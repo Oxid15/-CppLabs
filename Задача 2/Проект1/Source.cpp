8ae0e2065031f3f2f551486ec3e482db4274e8aa
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2017-09-18 00:27:07 +0500

    возможно даже конечная версия

--- Задача 2/Проект1/Source.cpp
+++ Задача 2/Проект1/Source.cpp
@@ -6,17 +6,20 @@ float a1, b1, c1, a2, b2, c2,x,y;
 
 int main()
 {
-	std::cout << "Write a1 \n"; std::cin >> a1;
-	std::cout << "Write b1 \n"; std::cin >> b1;
-	std::cout << "Write c1 \n"; std::cin >> c1;
-	std::cout << "Write a2 \n"; std::cin >> a2;
-	std::cout << "Write b2 \n"; std::cin >> b2;
-	std::cout << "Write c2 \n"; std::cin >> c2;
+	cout << "Write a1 \n"; cin >> a1;
+	cout << "Write b1 \n"; cin >> b1;
+	cout << "Write c1 \n"; cin >> c1;
+	cout << "Write a2 \n"; cin >> a2;
+	cout << "Write b2 \n"; cin >> b2;
+	cout << "Write c2 \n"; cin >> c2;
 
 	if ((abs(a1*b2 - a2*b1))>=0.0001)
 	
 	{
-		x == (b1*c2 - b2*c1)/(b2*a1-b1*a2)
-			y ==
+		x = (b2*c1 - b1*c2) / (b1*a2 - b2*a1);
+		y = ((a1*c2 - a2*c1) / (a2*b1 / a1*b2));
+		cout << "x=" << x << "\n";
+		cout << "y=" << y << "\n";
 	}
+	system("pause");
 }
