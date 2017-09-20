9897d343728f40d9e764cb7c83beb49ca4364537
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2017-09-20 12:53:13 +0500

    5 с практики

--- Задача 1/Source.cpp
+++ Задача 1/Source.cpp
@@ -1,4 +1,4 @@
-//вариант этой и последующих задач - 5
+//variant 5
 #include <iostream> 
 #include <math.h>
 using namespace std;
@@ -6,12 +6,12 @@ using namespace std;
 int x, y, z;
 int main()
 {
-	std::cout << "Write x -"; std::cin >> x;
-	std::cout << "Write y -"; std::cin >> y;
-	std::cout << "Write z -"; std::cin >> z;
+	cout << "Write x -"; cin >> x;
+	cout << "Write y -"; cin >> y;
+	cout << "Write z -"; cin >> z;
 
 	int a = log(abs((y - sqrt(abs(x))))*(x - (y / (z + x*x / 4))));
 	int b = (x - x*x /6)+(x*x*x*x*x/120);
-		std::cout <<"Variable a =" << a;
-		std::cout << "Variable b =" << b;
+		cout <<"Variable a =" << a;
+		cout << "Variable b =" << b;
 }
