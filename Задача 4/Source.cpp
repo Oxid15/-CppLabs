29d8b2ee4076b77c3bd7cd598e7cdf519d070999
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2017-09-23 21:37:03 +0500

    10

--- Задача 4/Source.cpp
+++ Задача 4/Source.cpp
@@ -1,29 +1,14 @@
 #include<iostream>
 using namespace std;
 
-int cnt=1;
+
 int main()
 {
-	int p,p1,x;
-	cout << "Write sequence that ends by 0: " << "\n"; cin >> p;
-	p1 = p;
-	while (p!=0)
+	cout << "Write sequence that ends by 0 character-by-character: " << "\n";
+	int x = 1;
+	while (x != 0)
 	{
-		p /= 10;
-		if (p != 0)
-		{
-			cnt = cnt * 10;
-			cout << " counter: ";
-		}
-		cout << cnt << "  " << p << "\n";
-	}
-	int x2;
-	while (cnt!=1)
-	{
-		x = p1 / cnt;
-		p1 = p1%cnt;
-		cnt /= 10;
-		cout << "x= " << x<< "\n";
+		cin >> x;
 	}
 	system("pause");
 }
