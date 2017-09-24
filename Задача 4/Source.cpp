4eed0b203aa429bde5923d93358de52afbdf1d99
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2017-09-24 22:51:15 +0500

    финальная версия для четвёртой задачи

--- Задача 4/Source.cpp
+++ Задача 4/Source.cpp
@@ -5,24 +5,26 @@ using namespace std;
 int main()
 {
 	cout << "Write sequence that ends by 0 character-by-character: " << "\n";
-	int xc=1, xp=1;
+	int xc=1, xp=1,cnt=0;
 	bool vv;
 	while (xc != 0)
 	{
 		xp = xc;
 		cin >> xc;
-		if ((xc < xp)&&(xc!=0))
+		cnt = cnt++;
+		if ((xc < xp)&&(xc!=0)&&(cnt=2))
 		{
 			vv = false;
-			cout << "nonincr"<<"\n";
 		}
 		else
 		{
-			vv = true;
-			cout << "incr"<< "\n";
+			if (xc != 0)
+			{
+				vv = true;
+			}
 		}
+		vv = vv*vv;
 	}
-	vv = vv*vv;
 	if (vv == 1)
 	{
 		cout << "the sequence is increasing" << "\n";
@@ -31,6 +33,5 @@ int main()
 	{
 		cout << "the sequence is nonincreasing" << "\n";
 	}
-	cout << vv;
 	system("pause");
 }
