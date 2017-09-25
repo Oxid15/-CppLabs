75bc581ec8a994edea070df8f9a9f4c5a999693d
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2017-09-25 22:59:34 +0500

    дополнение

--- Задача 6/Source.cpp
+++ Задача 6/Source.cpp
@@ -5,15 +5,14 @@ using namespace std;
 
 int main()
 {
-	int  i = 1;
 	double e,s = 0, sum = 0;
 	cout << "Write e" << "\n"; cin >> e;
-	for (i; s<e; i++)
+	for (int  i = 1; s<e; i++)
 	{
 		s = (pow((-1), (1 + i))) / (i*(i + 1)*(i + 2));
-		sum = sum + s;
-		cout << "p sum=" << sum << "\n";
+		sum += s;
+		cout << "mid sum=" << sum << "\n";
 	}
-	cout << "FinalSum=" << sum;
+	cout << "Final Sum=" << sum;
 	system("pause");
 }
