f965cdb4de4bcef194c7f2d8af2f0c2ccb359161
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2017-09-25 22:48:33 +0500

    что-то вышло

--- Задача 6/Source.cpp
+++ Задача 6/Source.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 
 int main()
 {
-	int i, e;
-	double sum=0;
-	cout << "Write i"; cin >> i;
-	cout << "Write e"; cin >> e;
-	for (i;i<e;i++)
+	int  i = 1;
+	double e,s = 0, sum = 0;
+	cout << "Write e" << "\n"; cin >> e;
+	for (i; s<e; i++)
 	{
-		sum = sum + (((-1) ^ (1 + i))/(i*(i + 1)*(i + 2)));
+		s = (pow((-1), (1 + i))) / (i*(i + 1)*(i + 2));
+		sum = sum + s;
+		cout << "p sum=" << sum << "\n";
 	}
-	cout << "Sum=" << sum;
+	cout << "FinalSum=" << sum;
+	system("pause");
 }
