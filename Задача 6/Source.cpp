87e66727ac2a9d071e7e3b81aafc8051ba54b548
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2017-09-25 21:03:55 +0500

    14

--- Задача 6/Source.cpp
+++ Задача 6/Source.cpp
@@ -5,10 +5,12 @@ using namespace std;
 int main()
 {
 	int i, e;
+	double sum=0;
 	cout << "Write i"; cin >> i;
 	cout << "Write e"; cin >> e;
-	for (i;;)
+	for (i;i<e;i++)
 	{
-
+		sum = sum + (((-1) ^ (1 + i))/(i*(i + 1)*(i + 2)));
 	}
+	cout << "Sum=" << sum;
 }
