7281de951dfd338d7cc4b9488ed8dd3b4c8aa697
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2017-09-30 16:22:54 +0500

    1

---
+++ ЛР2/Задача 1/Source.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	int n;
+	cout << "Write array size"<<"\n"; cin >> n;
+	const int n1 = 1;
+	int *n = new int[n1];
+	int arr[n1];
+	cout << n1;
+}
