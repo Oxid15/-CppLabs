a2fe849e9f36fa9f9261d96f145ae692c7a1671f
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2017-10-03 15:36:19 +0500

    всё ещё плохо с указателями, не смотря на это решил написать саму программу, в которой пока не работает ввод размерности массива с клавиатуры

--- ЛР2/Задача 1/Source.cpp
+++ ЛР2/Задача 1/Source.cpp
@@ -4,6 +4,18 @@ using namespace std;
 
 int main()
 {
-	int n;
+	int *m,n;
 	cout << "Write array size"<<"\n"; cin >> n;
+	*m & n;
+	int mas[n];
+	for (int a = 0; a != n; a + 2)
+	{
+		if ((mas[a] > mas[a-1])(a!=0))
+		{
+			for (int b = 0; b != n; b++)
+			{
+
+			}
+		}
+	}
 }
