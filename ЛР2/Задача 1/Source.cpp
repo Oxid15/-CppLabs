bcd9b0423156709d0f10b8e2ad3b6c78cb122e3f
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2017-10-03 15:54:55 +0500

    всё ещё смутно, но программа без ошибок
    пока что

--- ЛР2/Задача 1/Source.cpp
+++ ЛР2/Задача 1/Source.cpp
@@ -6,11 +6,10 @@ int main()
 {
 	int *m,n;
 	cout << "Write array size"<<"\n"; cin >> n;
-	*m & n;
-	int mas[n];
-	for (int a = 0; a != n; a + 2)
+	int *m = new int[n];
+	for (int a = 0; a != n; a + 2) 
 	{
-		if ((mas[a] > mas[a-1])(a!=0))
+		if ((m[a] > m[a-1])&&(a!=0))
 		{
 			for (int b = 0; b != n; b++)
 			{
@@ -18,4 +17,5 @@ int main()
 			}
 		}
 	}
+	delete m;
 }
