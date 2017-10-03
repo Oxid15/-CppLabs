c8194f1f5a80336372935790b404f499d2ee50cd
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2017-10-04 00:36:03 +0500

    условия задачи выполнены, но последние строчки кода не выполняются полностью по какой-то причине

--- ЛР2/Задача 1/Source.cpp
+++ ЛР2/Задача 1/Source.cpp
@@ -16,28 +16,46 @@ int main()
 		cout << "m[" << a << "]=" << m[a] << "\n";
 	}
 	int a = 3;
-	bool b = true;
+	bool b = true, d = true;
 	while (a <= n)
 	{
 		if (m[a] > m[a - 2])
+		{
 			b *= 1;
+		}
 		else
 		{
 			b = 0;
+			d = 1;
 			break;
 		}
 		a += 2;
 	}
 	int c = 0;
-	if (b = 1)
+	if (b == 1)
 	{
+		int i = 0;
 		while (c <= n)
 		{
 			sum = sum + m[c];
 			c += 2;
+			i = ++i;
 		}
-		cout << "sum=" << sum;
+		cout << "average=" << sum / i;
 	}
+		if (d == 1)
+		{
+			int cnt = 0, a = 0;
+			while (a <= n)
+			{
+				if (m[a] > 0)
+				{
+					cnt = ++cnt;
+					a = ++a;
+				}
+			}
+			cout << "positive numbers:" << cnt << "\n";
+		}
 	delete m;
 	system("pause");
 }
