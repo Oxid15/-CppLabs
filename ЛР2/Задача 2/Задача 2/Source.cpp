8e8c715b72257aaeea0db9365e9f173f4543260c
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2017-10-08 01:14:57 +0500

    По второй задаче: написал сортировку. Осталось выделить одинаковые максимумы/минимумы по заданию и написать для них необходимые вычисления.

---
+++ ЛР2/Задача 2/Задача 2/Source.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+
+int main()
+{
+	int n;
+	cout << "Write array size:" << "\n"; cin >> n;
+	int *m = new int[n];
+	srand(time(NULL));
+	for (int a = 0; a != n; a++)
+	{
+		m[a] = rand();
+		cout << "m[" << a << "]=" << m[a] << "\n";
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (m[j] < m[j + 1])
+			{
+				int a = 0;
+				a = m[j];
+				m[j] = m[j+1];
+				m[j+1] = a;
+			}
+		}	
+		cout << "m1[" << i << "]=" << m[i] << "\n";
+	}
+	system("pause");
+	delete[]m;
+}
