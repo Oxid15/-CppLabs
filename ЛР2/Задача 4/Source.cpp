ab5e32820bc1efd581c3930297ff8d6a7af7098a
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2017-10-09 19:00:29 +0500

    хотя дошло как работают двухмерные динамические массивы, не дошло как их использовать и писать

--- ЛР2/Задача 4/Source.cpp
+++ ЛР2/Задача 4/Source.cpp
@@ -4,12 +4,5 @@ using namespace std;
 
 int main()
 {
-	int m;
-	cout << "Write array size" << "\n"; cin >> m;
-	int **a = new int *[m];
-	for (int i = 0; i < m; i++)
-	{
-		int a[i] = new int[m];
-	}
 
 }
