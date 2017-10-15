0d9fadc30c96f52d01fbe39b939d3cee8db88df9
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2017-10-15 23:15:05 +0500

    1

--- ЛР2/Задача 2/Source.cpp
+++ ЛР2/Задача 2/Source.cpp
@@ -9,11 +9,22 @@ int main()
 	int n;
 	cout << "Write array size:" << "\n"; cin >> n;
 	int *m = new int[n];
-	srand(time(NULL));
-	for (int a = 0; a != n; a++)
+	if (n >= 10) 
 	{
-		m[a] = rand();
-		cout << "m[" << a << "]=" << m[a] << "\n";
+		srand(time(NULL));
+		for (int a = 0; a != n; a++)
+		{
+			m[a] = rand();
+			cout << "m[" << a << "]=" << m[a] << "\n";
+		}
+	}
+	else
+	{
+		for (int a = 0; a != n; a++)
+		{
+			cout << "Write m[" << a << "]" << "\n";
+			cin >> m[a];
+		}
 	}
 
 	system("pause");
