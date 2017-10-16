3e7e0ed3e82ecfb68dbef0d50a2e071b5ef64860
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2017-10-16 21:34:19 +0500

    2

--- ЛР2/Задача 4/Source.cpp
+++ ЛР2/Задача 4/Source.cpp
@@ -35,17 +35,6 @@ int main()
 			}
 		}
 	}
-	int* a = new int[];
-	for (int i = 0; i < n; i++)
-	{
-			if (i = 0)
-				a[0] = b[0][0];
-			else
-			{
-
-			}
-	}
-
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
@@ -54,11 +43,39 @@ int main()
 		}
 		cout << "\n";
 	}
+
+	int* a = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		if (i == 0)
+		{
+			a[i] = b[i][i];
+		}
+		else
+		{
+			break;
+		}
+		cout << "a[" << i << "]=" << a[i] << "\n";
+	}
+	/*
+	int x = 0;
+
+	while (int i = 0 >= x)
+	{
+		a[i] = b[i][i - x] + b[i - x][i];
+		i += 1;
+		for (int i = 0; i < n; i++)
+		{
+			cout << "a[" << i << "]=" << a[i] << "\n";
+		}
+	}
+	*/
+
 	system("pause");
-	
 	for (int i = 0; i < n; i++)
 	{
 			delete[]b[i];
 	}
 	delete[]b;
+	delete[]a;
 }
