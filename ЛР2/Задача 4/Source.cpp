d10b32a013089c02a179907e28c6f0a0483fb071
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2017-10-16 19:03:05 +0500

    ввод/вывод двухмерного динамического массива для четвёртой задачи

--- ЛР2/Задача 4/Source.cpp
+++ ЛР2/Задача 4/Source.cpp
@@ -4,5 +4,25 @@ using namespace std;
 
 int main()
 {
-
+	int n = 0;
+	cout << "Write matrix size:"; cin >> n;
+	int* *b = new int*[n];
+	for (int i = 0; i < n; i++)
+	{
+		b[i] = new int[n];
+		for (int j = 0; j < n; j++)
+		{
+			cout << "Write b[" << i << "]" << "[" << j << "]" << "/n";
+			cin >> b[i][j];
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << "b[" << i << "]" << "[" << j << "]=" << b[i][j] << "  ";
+		}
+		cout << "\n";
+	}
+	system("pause");
 }
