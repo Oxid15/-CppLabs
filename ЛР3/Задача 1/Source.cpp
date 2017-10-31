f0f6b3756712ead4bc063522b5c42588c4b09797
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2017-10-31 20:46:04 +0500

    2

--- ЛР3/Задача 1/Source.cpp
+++ ЛР3/Задача 1/Source.cpp
@@ -9,22 +9,27 @@ int main()
 	char* i = new char[256];
 
 	ifstream ifile("Input.txt");
-	if (!ifile) 
+	if (!ifile)
 	{
 		cout << "Input file error";
 	}
-	int count = 0, a[256];
+	int k = 0, count = 0, a[256];
 	for (int j = 0; j < 256; j++)
 	{
 		ifile.get(i,2);
-		if (ifile.eof())
-			break;
-		a[j] = atoi(i);
-		cout << "a[" << j << "]=" << a[j] << "\n";
-		count = ++count;
+		if (atoi(i) != 0)
+		{
+			a[k] = atoi(i);
+			cout << "a[" << k << "]=" << a[k] << "\n";
+			count = ++count;
+			k = ++k;
+		}
 	}
 	cout << "count=" << count;
 		ifile.close();
+	int el, elpas;
+	cout << "Write array element"; cin >> el;
+	cout << "Write array element to paste in output file"; cin >> elpas;
 
 	ofstream ofile("Output.txt");
 	if (!ofile)
