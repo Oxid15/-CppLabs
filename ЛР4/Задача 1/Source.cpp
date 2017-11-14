acf79e98f4ef30f5aa7b34385c824756566ae3d9
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2017-11-14 20:33:17 +0500

    3

--- ЛР4/Задача 1/Source.cpp
+++ ЛР4/Задача 1/Source.cpp
@@ -10,37 +10,33 @@ int main()
 	{
 		char* full_name = new char[256]; 
 		int group_num;
-		int grades[5];
+		int grade[5];
 	};
 	student a[10];
 	int count = 0;
+	ofstream ofile("lib.txt");
+
 	for (int i = 0; i < 10; i++)
 	{
-		cout << "Enter full name" << "\n"; cin >> a[i].full_name;
-		cout << "Enter number of group" << "\n"; cin >> a[i].group_num;
-		for (int j = 0; j < 5; j++)
+		cout << "Enter full name" << "\n"; 
+		cin >> a[i].full_name; 
+		ofile << a[i].full_name;
+		cout << "Enter number of group" << "\n"; 
+		cin >> a[i].group_num;
+		ofile << a[i].group_num;
+	/*	for (int j = 0; j < 5; j++)
 		{
 			cout << "Enter grade" << "\n";
-			cin >> a[i].grades[j];
-		}
+			cin >> a[j].grade;
+			ofile << a[j].grade[j];
+		}*/
+		ofile << "\n";
 		int k;
 			cout << "Enter 0 if you need to stop writing, or any positive number if you don't";
 			cin >> k;
 			count = ++count;
 			if (!k)
 				break;
-	}
-
-	for (int i = 0; i < count; i++)
-	{
-		cout << "Full name:" << a[i].full_name << "\n";
-		cout << "Group number:" << a[i].group_num << "\n";
-
-		for (int j = 0; j < 5; j++)
-		{
-			cout << i << " grade:" ;
-			cout << a[i].grades[j] << "\n";
-		}
 	}
 	system("pause");
 }
