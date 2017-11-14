a1ad1dce8e656b4dc735b656efd6ed3d2f050bdb
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2017-11-14 21:03:12 +0500

    ввод данных структуры в файл

--- ЛР4/Задача 1/Source.cpp
+++ ЛР4/Задача 1/Source.cpp
@@ -8,35 +8,42 @@ int main()
 {
 	struct student
 	{
-		char* full_name = new char[256]; 
+		char* first_name = new char[256];
+		char* last_name = new char[256];
+		char* patronymic_name = new char[256];
 		int group_num;
 		int grade[5];
 	};
 	student a[10];
-	int count = 0;
 	ofstream ofile("lib.txt");
 
 	for (int i = 0; i < 10; i++)
 	{
-		cout << "Enter full name" << "\n"; 
-		cin >> a[i].full_name; 
-		ofile << a[i].full_name;
+		cout << "Enter first name" << "\n"; 
+		cin >> a[i].first_name; 
+		ofile << a[i].first_name;
+		cout << "Enter last name" << "\n";
+		cin >> a[i].last_name;
+		ofile << a[i].last_name;
+		cout << "Enter patronymic name" << "\n";
+		cin >> a[i].patronymic_name;
+		ofile << a[i].patronymic_name;
 		cout << "Enter number of group" << "\n"; 
 		cin >> a[i].group_num;
 		ofile << a[i].group_num;
-	/*	for (int j = 0; j < 5; j++)
+		for (int j = 0; j < 5; j++)
 		{
 			cout << "Enter grade" << "\n";
-			cin >> a[j].grade;
-			ofile << a[j].grade[j];
-		}*/
+			cin >> a[i].grade[j];
+			ofile << a[i].grade[j];
+		}
 		ofile << "\n";
 		int k;
-			cout << "Enter 0 if you need to stop writing, or any positive number if you don't";
+			cout << "Enter 0 if you need to stop writing, or any positive number if you don't" << "\n";
 			cin >> k;
-			count = ++count;
 			if (!k)
 				break;
+			else
+				cout << "Continue..." << "\n";
 	}
-	system("pause");
 }
