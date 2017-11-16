e86abfc362746c4be3e6739eeae62a25f5e8ca81
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2017-11-16 18:39:11 +0500

    4

--- ЛР4/Задача 1/Source.cpp
+++ ЛР4/Задача 1/Source.cpp
@@ -15,35 +15,37 @@ int main()
 		int grade[5];
 	};
 	student a[10];
-	ofstream ofile("lib.txt");
-
+	int count = 0;
 	for (int i = 0; i < 10; i++)
 	{
-		cout << "Enter first name" << "\n"; 
-		cin >> a[i].first_name; 
-		ofile << a[i].first_name;
+		cout << "Enter first name" << "\n";
+		cin >> a[i].first_name;
 		cout << "Enter last name" << "\n";
 		cin >> a[i].last_name;
-		ofile << a[i].last_name;
 		cout << "Enter patronymic name" << "\n";
 		cin >> a[i].patronymic_name;
-		ofile << a[i].patronymic_name;
-		cout << "Enter number of group" << "\n"; 
+		cout << "Enter number of group" << "\n";
 		cin >> a[i].group_num;
-		ofile << a[i].group_num;
 		for (int j = 0; j < 5; j++)
 		{
 			cout << "Enter grade" << "\n";
 			cin >> a[i].grade[j];
-			ofile << a[i].grade[j];
 		}
-		ofile << "\n";
+		count = ++count;
 		int k;
-			cout << "Enter 0 if you need to stop writing, or any positive number if you don't" << "\n";
-			cin >> k;
-			if (!k)
-				break;
-			else
-				cout << "Continue..." << "\n";
+		cout << "Enter 0 if you need to stop writing, or any positive number if you don't" << "\n";
+		cin >> k;
+		if (!k)
+			break;
+		else
+			cout << "Continue..." << "\n";
 	}
+	for (int i = 0; i < count; i++)
+	{
+		cout << a[i].first_name << "\n";
+		cout << a[i].last_name << "\n";
+		cout << a[i].patronymic_name << "\n";
+		cout << a[i].group_num << "\n";
+	}
+	system("pause");
 }
