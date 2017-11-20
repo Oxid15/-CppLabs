1b7c628071af5c4bbae02478626c1e8a4f0bb27d
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2017-11-20 20:43:04 +0500

    написан первый вариант работы, использующий массив

--- ЛР4/Задача 1/Source.cpp
+++ ЛР4/Задача 1/Source.cpp
@@ -13,6 +13,7 @@ int main()
 		char* patronymic_name = new char[256];
 		int group_num;
 		int grade[5];
+		double avg;
 	};
 	student a[10];
 	int count = 0;
@@ -26,13 +27,18 @@ int main()
 		cin >> a[i].patronymic_name;
 		cout << "Enter number of group" << "\n";
 		cin >> a[i].group_num;
-		for (int j = 0; j < 5; j++)
+		double sum = 0;
+     	for (int j = 0; j < 5; j++)
 		{
 			cout << "Enter grade" << "\n";
 			cin >> a[i].grade[j];
+			sum = sum + a[i].grade[j];
+			cout << "sum=" << sum << "\n";
 		}
+		a[i].avg = sum / 5;
+		cout << "avg=" << a[i].avg << "\n";
 		count = ++count;
-		int k;
+		double k;
 		cout << "Enter 0 if you need to stop writing, or any positive number if you don't" << "\n";
 		cin >> k;
 		if (!k)
@@ -40,12 +46,22 @@ int main()
 		else
 			cout << "Continue..." << "\n";
 	}
+
+	for (int i = 0; i < count; i++)
+	{
+		for (int j = 0; j < count; j++)
+		{
+			if (a[j].avg < a[j + 1].avg)
+				swap(a[j], a[j + 1]);
+		}
+	}
 	for (int i = 0; i < count; i++)
 	{
 		cout << a[i].first_name << "\n";
 		cout << a[i].last_name << "\n";
 		cout << a[i].patronymic_name << "\n";
 		cout << a[i].group_num << "\n";
+		cout << a[i].avg << "\n";
 	}
 	system("pause");
 }
