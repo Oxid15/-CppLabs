88ecc46efa739853f9e70b4fc342905804a7d4c0
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2017-11-29 15:28:09 +0500

    чистка репо (надеюсь успешная)

---
+++ ЛР4/Задача 1/Source.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+int main()
+{
+	struct student
+	{
+		char* first_name = new char[256];
+		char* last_name = new char[256];
+		char* patronymic_name = new char[256];
+		int group_num;
+		int grade[5];
+		double avg;
+	};
+	student a[10];
+	int count = 0;
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "Enter first name" << "\n";
+		cin >> a[i].first_name;
+		cout << "Enter last name" << "\n";
+		cin >> a[i].last_name;
+		cout << "Enter patronymic name" << "\n";
+		cin >> a[i].patronymic_name;
+		cout << "Enter number of group" << "\n";
+		cin >> a[i].group_num;
+		double sum = 0;
+     	for (int j = 0; j < 5; j++)
+		{
+			cout << "Enter grade" << "\n";
+			cin >> a[i].grade[j];
+			sum = sum + a[i].grade[j];
+			cout << "sum=" << sum << "\n";
+		}
+		a[i].avg = sum / 5;
+		cout << "avg=" << a[i].avg << "\n";
+		count = ++count;
+		double k;
+		cout << "Enter 0 if you need to stop writing, or any positive number if you don't" << "\n";
+		cin >> k;
+		if (!k)
+			break;
+		else
+			cout << "Continue..." << "\n";
+	}
+
+	for (int i = 0; i < count; i++)
+	{
+		for (int j = 0; j < count; j++)
+		{
+			if (a[j].avg < a[j + 1].avg)
+				swap(a[j], a[j + 1]);
+		}
+	}
+	for (int i = 0; i < count; i++)
+	{
+		cout << a[i].first_name << "\n";
+		cout << a[i].last_name << "\n";
+		cout << a[i].patronymic_name << "\n";
+		cout << a[i].group_num << "\n";
+		cout << a[i].avg << "\n";
+	}
+	system("pause");
+}
