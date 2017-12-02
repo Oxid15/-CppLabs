0c8982b54dd992f6a9d6f9bd360f39b36cf65815
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2017-12-02 23:58:07 +0500

    слишком сложное решение мне кажется

--- ЛР3/extra/Source.cpp
+++ ЛР3/extra/Source.cpp
@@ -6,36 +6,90 @@ using namespace std;
 
 int main()
 {
-	cout << (int) ';' << "\n"; // 59
 	ifstream ifile("Input.txt");
 	if (!ifile)
 	{
 		cout << "Input file error";
 	}
-	int n = 0, x = 0;
+	int x = 0, lenof = 0;
 	int* a = new int[256];
 	while (!ifile.eof())
 	{
 		int buff = ifile.get() - 48;
-		if (buff < 10 && buff >= 0 || buff == 11 && x > 3)
+		if (buff < 10 && buff >= 0 || buff == 11 || buff == -38)
 		{
 			a[x] = buff;
 			x++;
 		}
+		lenof++;
 	}
-	for (int j = 0; j < x; j++)
+	//for (int j = 0; j < x; j++)
+	//{
+	//	cout << "a[" << j << "]= " << a[j] << "\n";
+	//}
+
+	int* b = new int[x];
+	int j = 0;
+	for (int i = 0; i < x; i++)
 	{
-		cout << "a[" << j << "]= " << a[j] << "\n";
+		if (a[i] == 11 && a[i+1] == 11 && a[i+2] == 11)
+		{
+			i = i + 3;
+		}
+		if (a[i] == -38)
+		{
+			b[j] = 11;
+			j++;
+		}
+		else
+		{
+			b[j] = a[i];
+			j++;
+		}
 	}
-
-	ifile.close();
-
-	ofstream ofile("Output.txt");
-	if (!ofile)
+	for (int k = 0; k < j; k++)
 	{
-		cout << "Output file error";
+		cout << "b[" << k << "]= " << b[k] << "\n";
+	}
+	cout << "j =" << j << "\n";
+	int* temp = new int[j];
+	int temp_len = 0;
+	for (int i = 0; i < j; i++)
+	{
+		temp[i] = 0;
+	}
+	int cnt = 0;
+	for (int i = 0; i < j; i++)
+	{
+		if (b[i] != 11)
+		{
+			temp[cnt] = temp[cnt]*10 + b[i];
+			cout << cnt << " - " << temp[cnt] << "\n";
+		}
+		else
+		{
+			cnt++;
+		}
+		temp_len++;
+	}
+	for (int k = 0; k < cnt; k++)
+	{
+		cout << "temp[" << k << "]= " << temp[k] << "\n";
+	}
+	cout << "temp length = " << temp_len << "\n";
+	int* yrs = new int[temp_len];
+	for (int i = 0; i < temp_len; i = i + 2)
+	{
+			yrs[i] = temp[i];
 	}
 
-	ofile.close();
+	for (int k = 0; k < temp_len; k++)
+	{
+		cout << "yrs[" << k << "]= " << yrs[k] << "\n";
+	}
+	//for (int k = 0; k < len; k++)
+	//{
+	//	cout << "mny[" << k << "]= " << mny[k] << "\n";
+	//}
 	system("pause");
 }
