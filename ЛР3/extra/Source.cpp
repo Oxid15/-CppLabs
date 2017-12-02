b3281cc023b640a3a52d75795cfba163f939417b
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2017-12-03 03:55:40 +0500

    задача решена, решение очень громоздкое

--- ЛР3/extra/Source.cpp
+++ ЛР3/extra/Source.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
 using namespace std;
 
 
@@ -11,25 +12,22 @@ int main()
 	{
 		cout << "Input file error";
 	}
-	int x = 0, lenof = 0;
-	int* a = new int[256];
+	int x = 0, g = 0;
+	int* a = new int[256],* orig = new int[512];
 	while (!ifile.eof())
 	{
 		int buff = ifile.get() - 48;
+		orig[g] = buff;
 		if (buff < 10 && buff >= 0 || buff == 11 || buff == -38)
 		{
 			a[x] = buff;
 			x++;
 		}
-		lenof++;
+		g++;
 	}
-	//for (int j = 0; j < x; j++)
-	//{
-	//	cout << "a[" << j << "]= " << a[j] << "\n";
-	//}
 
 	int* b = new int[x];
-	int j = 0;
+	int lentmp = 0;
 	for (int i = 0; i < x; i++)
 	{
 		if (a[i] == 11 && a[i+1] == 11 && a[i+2] == 11)
@@ -38,44 +36,34 @@ int main()
 		}
 		if (a[i] == -38)
 		{
-			b[j] = 11;
-			j++;
+			b[lentmp] = 11;
+			lentmp++;
 		}
 		else
 		{
-			b[j] = a[i];
-			j++;
+			b[lentmp] = a[i];
+			lentmp++;
 		}
 	}
-	//for (int k = 0; k < j; k++)
-	//{
-	//	cout << "b[" << k << "]= " << b[k] << "\n";
-	//}
-	//cout << "j =" << j << "\n";
-	int* temp = new int[j];
-	for (int i = 0; i < j; i++)
+	delete[]a;
+	int* temp = new int[lentmp];
+	for (int i = 0; i < lentmp; i++)
 	{
 		temp[i] = 0;
 	}
 	int cnt = 0;
-	for (int i = 0; i < j; i++)
+	for (int i = 0; i < lentmp; i++)
 	{
 		if (b[i] != 11)
 		{
 			temp[cnt] = temp[cnt]*10 + b[i];
-			//cout << cnt << " - " << temp[cnt] << "\n";
 		}
 		else
 		{
 			cnt++;
-			//cout << "cnt_e= " << cnt << "\n" ;
 		}
 	}
-	//for (int k = 0; k < cnt; k++)
-	//{
-	//	cout << "temp[" << k << "]= " << temp[k] << "\n";
-	//}
-	//cout << "temp length = " << cnt << "\n";
+	delete[]b;
 	int* yrs = new int[cnt], * mny = new int[cnt];
 	int yrslen = 0, mnylen = 0, in = 0;
 	for (int i = 0, j =0, k = 1; i < cnt; i++, j = j + 2, k = k + 2)
@@ -101,18 +89,7 @@ int main()
 			mnylen++;
 		}
 	}
-
-	//for (int k = 0; k < yrslen; k++)
-	//{
-	//	cout << "yrs[" << k << "]= " << yrs[k] << "\n";
-	//}
-	//cout << "\n" << "\n";
-	//for (int k = 0; k < mnylen; k++)
-	//{
-	//	cout << "mny[" << k << "]= " << mny[k] << "\n";
-	//}
-
-	////finally.... finding maximum of wages
+	delete[] temp;
 	int mnymax = 0, index = 0;
 	for (int i = 0; i < mnylen; i++)
 	{
@@ -121,7 +98,41 @@ int main()
 			mnymax = mny[i];
 			index = i;
 		}
-		cout << index;
 	}
-	system("pause");
+	delete[] mny;
+	delete[] yrs;
+	ifile.close();
+
+	ofstream ofile("Output.txt");
+		int count = 0, count2 = 0;
+	for (int i = 0; i < g; i++)
+	{
+		for (int i = 0; i < g; i++)
+		{
+			if (orig[i] == -38)
+				count++;
+
+			if (count == index)
+			{
+				if (orig[i] == -38)
+					i++;
+				if (orig[i] == 11)
+					count2++;
+				if (count2 < 2)
+				{
+					if (orig[i] == 11)
+					{
+						ofile << " ";
+						i++;
+					}
+					ofile << (char)(orig[i] + 16);
+				}
+
+			}
+		}
+	}
+	ofile << " - worker with a biggest wage and younger than 35" << "\n";
+	ofile.close();
+	delete[] orig;
+//	system("pause");
 }
