f96445a9071a81a3f8b4dbb136bc02ec40939386
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2018-02-21 21:58:21 +0500

    почти ничего, пытаюсь придумать что делать с этим

---
+++ SnakeTask/Source.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+class Point 
+{
+    public: short x, y; 
+    public:	char sym;
+
+	void Draw(short x1, short y1, char sym1)
+	{
+
+	}
+};
+
+void main()
+{
+	
+}
+
+
+
+
