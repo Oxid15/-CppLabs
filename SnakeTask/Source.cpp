044dd293af86c3c9577795279c6b19cae74c0410
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2018-02-28 17:47:48 +0500

    2

--- SnakeTask/Source.cpp
+++ SnakeTask/Source.cpp
@@ -2,20 +2,65 @@
 
 using namespace std;
 
-class Point 
+bool gameover;
+const int width = 20, height = 20;
+int x, y, fruitX, fruitY, score;
+enum direction {STOP, LEFT, RIGHT, UP, DOWN};
+direction dir;
+void Setup()
 {
-    public: short x, y; 
-    public:	char sym;
+	dir = STOP;
+	x = width / 2;
+	y = height / 2;
+	fruitX = rand() % width;
+	fruitY = rand() % height;
+	score = 0;
+}
 
-	void Draw(short x1, short y1, char sym1)
+
+void Draw()
+{
+	system ("cls");
+	for (int i = 0; i < width + 1; i++)
 	{
-
+		cout << "#";
 	}
-};
+	cout << "\n";
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			if (j == 0 || j == width - 1)
+				cout << "#";
+			cout << " ";
+		}
+		cout << "\n";
+	}
+	for (int i = 0; i < width + 1; i++)
+	{
+		cout << "#";
+	}
+}
+
+void Input()
+{
+
+}
+
+void Logic()
+{
+
+}
 
 void main()
 {
-	
+	Setup();
+	while (!gameover)
+	{
+		Draw();
+		Input();
+		Logic();
+	}
 }
 
 
