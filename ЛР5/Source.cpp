6f5644a82fd5af96ea3e7b938276bda34e11b703
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2018-03-05 20:19:54 +0500

    2

--- ЛР5/Source.cpp
+++ ЛР5/Source.cpp
@@ -9,12 +9,13 @@ int labyrinth[9][9] =
 { 1,1,0,0,0,0,1,1,1 },
 { 1,1,0,1,1,1,1,1,1 },
 { 1,1,0,1,1,1,1,1,1 },
+{ 1,1,0,1,1,1,1,1,1 },
 { 1,1,0,0,0,1,1,1,1 },
 { 1,1,1,1,0,1,1,1,1 },
 { 1,1,1,1,0,1,1,1,1 }
 };
 
-struct Coordinates
+struct coordinates
 {
 	int x;
 	int y;
@@ -22,5 +23,36 @@ struct Coordinates
 
 class Stack
 {
-	
+    coordinates arr[60];
+	coordinates* head = new coordinates;
+
+    public: void push(coordinates crd)
+	{	
+		head = arr;
+	    *head = crd;
+		head ++;
+	}
+
+	public: void pop()
+	{
+
+	}
+
+    public: void peek()
+	{
+		cout << " ";
+	}
 };
+
+
+int main()
+{
+	Stack stack1;
+	coordinates c0;
+	c0.x = 5;
+	c0.y = 9;
+	stack1.push(c0);
+
+	system("pause");
+	return 0;
+}
