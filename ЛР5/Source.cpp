6f622958bc444d4797a8798ab6e6f6a1e9bb274a
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2018-03-05 20:42:39 +0500

    3

--- ЛР5/Source.cpp
+++ ЛР5/Source.cpp
@@ -24,35 +24,43 @@ struct coordinates
 class Stack
 {
     coordinates arr[60];
-	coordinates* head = new coordinates;
+    coordinates* head = new coordinates;
 
-    public: void push(coordinates crd)
-	{	
-		head = arr;
-	    *head = crd;
-		head ++;
-	}
+public: void stackInit()
+{
+	head = arr;
+	head--;
+}
 
-	public: void pop()
-	{
+public: void push(coordinates crd)
+{
+	head++;
+	*head = crd;
+}
 
-	}
+public: void pop()
+{
 
-    public: void peek()
-	{
-		cout << " ";
-	}
+}
+
+public: void peek()
+{
+	cout << " ";
+}
 };
 
 
 int main()
 {
 	Stack stack1;
-	coordinates c0;
+	stack1.stackInit();
+	coordinates c0, c1;
 	c0.x = 5;
 	c0.y = 9;
 	stack1.push(c0);
-
+	c1.x = 5;
+	c1.y = 8;
+	stack1.push(c1);
 	system("pause");
 	return 0;
 }
