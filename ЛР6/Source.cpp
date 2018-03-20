ee4bda2d4ee435707646838e4232afbd440f43e9
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2018-03-20 21:54:51 +0500

    2

---
+++ ЛР6/Source.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+
+class ComplexNumber
+{
+private:
+	//float x, y;
+public:
+	float x, y;
+	void numberInput()
+	{
+		bool isRight = false;
+		//while (!isRight)
+		//{
+			float inputX, inputY;
+			cout << "Write x and y through the gap or press Enter after first number" << "\n";
+			cin >> inputX >> inputY;
+			x = inputX;
+			y = inputY;
+			//isRight = true;
+			system("pause");
+			system("cls");
+		//} //будущая проверка ввода
+	}
+	
+	const ComplexNumber operator+(const ComplexNumber second) const
+	{
+		ComplexNumber result;
+		result.x = x + second.x;
+		result.y = y + second.y;
+		return (result);
+	}
+
+	const ComplexNumber operator-(const ComplexNumber second) const
+	{
+		ComplexNumber result;
+		result.x = x - second.x;
+		result.y = y - second.y;
+		return (result);
+	}
+};
+
+void main()
+{
+	ComplexNumber a, b, c;
+	a.x = 4;
+	a.y = 3;
+	b.x = 1;
+	b.y = 2;
+	c = a + b;
+	cout << c.x << c.y;
+	cout << "\n";
+	c = a - b;
+	cout << c.x << c.y;
+	system("pause");
+}
