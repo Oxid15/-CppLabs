09d80aab0084bfcb2fb48bd0e93a940c9b84bc4e
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2018-03-30 22:52:02 +0500

    1.1

--- ЛР7/Source.cpp
+++ ЛР7/Source.cpp
@@ -4,35 +4,43 @@ using namespace std;
 
 class BaseClass
 {
-	virtual bool equals()
-	{
-
-	}
+	virtual bool equals(BaseClass &inst)
+	{}
 
 	virtual void toString(char *buffer)
-	{
-
-	}
+	{}
 };
 
 class Container : BaseClass
 {
 	BaseClass** array = new BaseClass*;
+	bool isFirst;
+
+	void addContainer(int &i, BaseClass* &arr)
+	{
+
+	}
+
+	void addElement()
+	{
+
+	}
 };
 
 class Element : BaseClass
 {
-	int n;
-	union Data
-	{
-		int intData;
-		float floatData;
-		bool boolData;
-	};
-	Data* vector = new Data[n];
+	Data dataElem;
+};
+
+union Data
+{
+	int intData;
+	float floatData;
+	bool boolData;
 };
 
 void main()
 {
-
+	int i = 0;
+	BaseClass* baseArr = new BaseClass;
 }
