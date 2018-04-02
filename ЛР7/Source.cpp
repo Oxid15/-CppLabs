7201db2c1474d7339259b33f6366a384294a25ff
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2018-04-02 22:58:00 +0500

    1.2

--- ЛР7/Source.cpp
+++ ЛР7/Source.cpp
@@ -5,31 +5,41 @@ using namespace std;
 class BaseClass
 {
 	virtual bool equals(BaseClass &inst)
-	{}
+	{
+		return false;
+	}
 
-	virtual void toString(char *buffer)
+	virtual void toString(char *buffer)			 //?
 	{}
 };
 
 class Container : BaseClass
 {
 	BaseClass** array = new BaseClass*;
-	bool isFirst;
-
-	void addContainer(int &i, BaseClass* &arr)
+	Container* next = nullptr;
+	Container* prev = nullptr;					  //?
+	int j = 0;
+public:
+	void addFirstContainer(int &i, BaseClass** &baseArr)
 	{
+		Container firstContainer;
+		baseArr[i] = &firstContainer;
+		i++;
+	}
 
+	void addContainer()
+	{
+		Container newContainer;
+		array[j] = &newContainer;
+		j++;
+		next = &newContainer;
+		//prev = & ?
 	}
 
 	void addElement()
 	{
 
 	}
-};
-
-class Element : BaseClass
-{
-	Data dataElem;
 };
 
 union Data
@@ -39,8 +49,18 @@ union Data
 	bool boolData;
 };
 
+class Element : BaseClass
+{
+	Data dataElem;
+};
+
 void main()
 {
 	int i = 0;
-	BaseClass* baseArr = new BaseClass;
+	BaseClass** baseArr = new BaseClass*[8]; //?
+	Container* current;
+	Container c;
+	current = &c;
+	c.addFirstContainer(i,baseArr);
+	c.addContainer();
 }
