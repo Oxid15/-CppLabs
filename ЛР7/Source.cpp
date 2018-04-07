e59e1e965b4889a1a179a9bc49979f93b27eebd4
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2018-04-07 20:51:34 +0500

    2

--- ЛР7/Source.cpp
+++ ЛР7/Source.cpp
@@ -6,39 +6,7 @@ class BaseClass
 {													
 	virtual bool equals(BaseClass &inst) = 0;
 	virtual void toString(char *buffer) = 0;
-};
-
-class Element : BaseClass
-{
-	Data dataElem[/*размер*/];								//без повтор€ющихс€ элементов. поиск?
-
-	bool equals(BaseClass &inst) { return false; }
-	void toString(char *buffer) { }
-};
-
-class Container : BaseClass
-{										
-	BaseClass** array = new BaseClass*[/*размер*/];          //динамическое расширение?
-	Container* parent = nullptr;					  
-	int j = 0;
-public:
-	void addContainer()
-	{
-		Container* newContainer = new Container;
-		array[j] = (BaseClass*) newContainer;
-		j++;
-		newContainer->parent = this;								         //указатель на текущий объект?
-	}
-
-	void addElement()
-	{
-		Element* newElem = new Element;
-		array[j] = (BaseClass*) newElem;						         //ошибка
-		j++;
-	}
-
-	bool equals(BaseClass &inst) { return false; }
-	void toString(char *buffer) { }
+	virtual int getType() = 0;
 };
 
 union Data
@@ -48,6 +16,59 @@ union Data
 	bool boolData;
 };
 
+class Element : BaseClass
+{
+	Data dataElem;							
+
+	bool equals(BaseClass &inst) { return false; }
+	void toString(char *buffer) 
+	{ }
+	int getType()
+	{
+		return 1;
+	}
+};
+
+class Container : BaseClass
+{
+	int len = 8;
+	BaseClass** array = new BaseClass*[len];      
+	Container* parent = nullptr;					  
+	int j = 0;
+public:
+	int getType()
+	{
+		return 0;
+	}
+	void addContainer()
+	{
+		Container* newContainer = new Container;
+		array[j] = (BaseClass*) newContainer;
+		j++;
+		newContainer->parent = this;								       
+	}
+
+	void addElement()
+	{
+		Element* newElem = new Element;
+		array[j] = (BaseClass*) newElem;						        
+		j++;
+	}
+
+	bool equals(BaseClass &inst) 
+	{
+		return false; 
+	}
+
+	void toString(char *buffer) 
+	{
+		for (int i = 0; i < len; i++)
+		{
+
+		}
+	}
+};
+
 void main()
 {
 	int i = 0;
@@ -55,4 +76,11 @@ void main()
 	Container c;
 	current = &c;
 	c.addContainer();
+	char* buff = new char[8];
+	c.toString(buff);
+	for (int i = 0; i < strlen(buff); i++)
+	{
+		cout << buff[i] << " ";
+	}
+	system("pause");
 }
