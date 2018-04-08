18dd7b546f17bc8bf111a5b8d299928a6a37440a
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2018-04-09 00:33:56 +0500

    2.2

--- ЛР7/Source.cpp
+++ ЛР7/Source.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class BaseClass
 {													
 	virtual bool equals(BaseClass &inst) = 0;
-	virtual void toString(char *buffer) = 0;
+	virtual char* toString(char *buffer) = 0;
 	virtual int getType() = 0;
 };
 
@@ -26,9 +26,9 @@ class Element : BaseClass
 			return false;
 	}
 
-	void toString(char *buffer) 
+	char* toString(char *buffer) 
 	{
-
+		return buffer;
 	}
 	int getType()
 	{
@@ -48,7 +48,7 @@ public:
 		j = 0;
 		len = 8;
 	}
-
+	
 	int getType()
 	{
 		return 0;
@@ -61,7 +61,7 @@ public:
 		j++;
 		newContainer->parent = this;								       
 	}
-
+	
 	void addElement()
 	{
 		Element* newElem = new Element;
@@ -77,16 +77,44 @@ public:
 		//...
 	}
 
-	void toString(char *buffer) 
+	char* toString(char *buffer)
 	{
 		for (int i = 0; i < len; i++)
 		{
 			//...
 		}
+		return buffer;
 	}
 };
 
 void main()
 {
-	system("pause");
+	Container root;
+	Container* current = new Container;
+	current = &root;
+	while (1)
+	{
+		int choice;
+		cout << "1 - addContainer\n 2 - addElement\n 3- toString";
+		cin >> choice;
+		switch (choice)
+		{
+		case 1:
+			current->addContainer();
+		case 2:
+			current->addElement();
+		case 3:
+			char* buf = new char[255];
+			current->toString(buf);
+	  /*
+	     case 4:
+	        current->equals(current);
+	     case 5:
+	        current->moveDown(current);
+		 case 6:
+		    current-> moveUp(current);
+	  */
+		}
+			system("pause");
+	}
 }
