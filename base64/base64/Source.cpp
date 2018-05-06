e505ede346d008e036832af7441463ef2c9db5c9
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2018-05-07 01:21:52 +0500

    1

--- base64/base64/Source.cpp
+++ base64/base64/Source.cpp
@@ -1,15 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<fstream>
+#include<iostream>
 
 using namespace std;
 
+//enum code { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, 
+//	        a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z};
+
 void fileInput(int* arr, int &i)
 {
-	ifstream iFile("input.txt", ios::binary);
-	while(!iFile.eof())
+	ifstream file("input.txt", ios::binary);
+	while(!file.eof())
 	{
-		arr[i] = iFile.get();
+		arr[i] = file.get();
 		i++;
 	}
+	file.close();
+}
+
+void fileOutput(int * arr, int size)
+{
+	ofstream file("output.txt");
+	for (int i = 0; i < size; i++)
+	{
+		//char* c = new char;
+		//_itoa(arr[i], c, 10);								 //!
+		char c = arr[i] + 65;
+		file << c;
+	}
+	file.close();
 }
 
 void main()
@@ -17,12 +36,63 @@ void main()
 	int counter = 0;
 	int arr[2048];
 	fileInput(arr,counter);
-
-	int addCount = 0;
-	while (counter % 6 == 0)
+	int size = counter * 8;
+	int numOfEights = 0;
+	
+	while (size % 6 != 0)
 	{
-		counter += 8;
-		addCount++;
+		size += 8;
+		numOfEights++;
 	}
 
+	int* result = new int[counter * 6];
+
+	for (int i = 0, s = 0; i < counter; i += 3, s += 4)
+	{
+		result[s] = (arr[i] & 252) >> 2;
+	}
+
+	for (int i = 0 ,j = 1, s = 1; j < counter; i += 3, j += 3, s += 4)
+	{
+		result[s] = ((arr[i] & 3) << 4) + ((arr[j] & 240) >> 4);
+	}
+
+	for (int i = 1, j = 2, s = 2; j < counter; i += 3, j += 3, s += 4)
+	{
+		result[s] = ((arr[i] & 15) << 2) + ((arr[j] & 192) >> 6);
+	}
+
+	for (int i = 2, s = 3; i < counter; i += 3, s += 4)
+	{
+		result[s] = arr[i] & 63;
+	}
+
+	fileOutput(result, counter * 6);
 }
+
+//void main()
+//{
+//	int a = 77, b = 97;
+//	int c = a & 3;
+//	int d = b & 240;
+//	int e = c << 4;
+//	int f = d >> 4;
+//	int g = e + f;
+//}
+
+
+//void main()
+//{
+//	ofstream ofile ("output.txt");
+//	for (int i = 0; i < 10000; i += 2)
+//	{
+//		int result = i;
+//		int counter = 0;
+//		while (result % 6 != 0)
+//		{
+//			result += 8;
+//			counter++;
+//		}
+//		ofile << i << ": " << counter << " of 8 = " << result << "\n";
+//	}
+//}
