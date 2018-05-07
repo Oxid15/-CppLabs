afa245f71737adc62cad086d19dc28a09168d93e
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2018-05-07 16:40:28 +0500

    2

--- base64/base64/Source.cpp
+++ base64/base64/Source.cpp
@@ -1,12 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<fstream>
 #include<iostream>
+#include <bitset>
 
 using namespace std;
 
-//enum code { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, 
-//	        a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z};
-
+char code[] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"};
 void fileInput(int* arr, int &i)
 {
 	ifstream file("input.txt", ios::binary);
@@ -15,18 +14,16 @@ void fileInput(int* arr, int &i)
 		arr[i] = file.get();
 		i++;
 	}
+	i--;
 	file.close();
 }
 
-void fileOutput(int * arr, int size)
+void fileOutput(int * arr, int size, char* code)
 {
 	ofstream file("output.txt");
 	for (int i = 0; i < size; i++)
 	{
-		//char* c = new char;
-		//_itoa(arr[i], c, 10);								 //!
-		char c = arr[i] + 65;
-		file << c;
+		file << code[arr[i]];
 	}
 	file.close();
 }
@@ -47,52 +44,43 @@ void main()
 
 	int* result = new int[counter * 6];
 
-	for (int i = 0, s = 0; i < counter; i += 3, s += 4)
+	for (int i = 0, s = 0; s < counter; i += 3, s += 4)
 	{
 		result[s] = (arr[i] & 252) >> 2;
 	}
 
-	for (int i = 0 ,j = 1, s = 1; j < counter; i += 3, j += 3, s += 4)
+	for (int i = 0 ,j = 1, s = 1; s < counter; i += 3, j += 3, s += 4)
 	{
 		result[s] = ((arr[i] & 3) << 4) + ((arr[j] & 240) >> 4);
 	}
 
-	for (int i = 1, j = 2, s = 2; j < counter; i += 3, j += 3, s += 4)
+	for (int i = 1, j = 2, s = 2; s < counter; i += 3, j += 3, s += 4)
 	{
 		result[s] = ((arr[i] & 15) << 2) + ((arr[j] & 192) >> 6);
 	}
 
-	for (int i = 2, s = 3; i < counter; i += 3, s += 4)
+	for (int i = 2, s = 3; s < counter; i += 3, s += 4)
 	{
 		result[s] = arr[i] & 63;
 	}
 
-	fileOutput(result, counter * 6);
+	fileOutput(result, counter, code);
+	//system("pause");
 }
 
-//void main()
-//{
-//	int a = 77, b = 97;
-//	int c = a & 3;
-//	int d = b & 240;
-//	int e = c << 4;
-//	int f = d >> 4;
-//	int g = e + f;
-//}
+//Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure.
+//AAAA = QUFBQQ==
 
-
-//void main()
+//cout << "array: ";
+//for (int k = 0; k < counter; k++)
 //{
-//	ofstream ofile ("output.txt");
-//	for (int i = 0; i < 10000; i += 2)
-//	{
-//		int result = i;
-//		int counter = 0;
-//		while (result % 6 != 0)
-//		{
-//			result += 8;
-//			counter++;
-//		}
-//		ofile << i << ": " << counter << " of 8 = " << result << "\n";
-//	}
+//	cout << bitset<8>(arr[k]) << " , ";
 //}
+//cout << "\n";
+//cout << "\n";
+//cout << "return: ";
+//for (int k = 0; k < counter; k++)
+//{
+//	cout << bitset<8>(result[k]) << " , ";
+//}
+//cout << "\n";
