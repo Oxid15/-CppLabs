6a57edf27e649929395fc72e91ad15be28169124
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2018-05-07 17:53:30 +0500

    3

--- base64/base64/Source.cpp
+++ base64/base64/Source.cpp
@@ -15,15 +15,24 @@ void fileInput(int* arr, int &i)
 		i++;
 	}
 	i--;
+	arr[i] = 0;
 	file.close();
 }
 
-void fileOutput(int * arr, int size, char* code)
+void fileOutput(int * arr, int size, char* code, int eights)
 {
 	ofstream file("output.txt");
 	for (int i = 0; i < size; i++)
 	{
+		if (i < size - 2)
 		file << code[arr[i]];
+		else
+		{
+			if (arr[i] == 0)
+				file << '=';
+			else
+				file << code[arr[i]];
+		}
 	}
 	file.close();
 }
@@ -32,6 +41,8 @@ void main()
 {
 	int counter = 0;
 	int arr[2048];
+	for (int i = 0; i < 2048; i++)
+		arr[i] = 0;
 	fileInput(arr,counter);
 	int size = counter * 8;
 	int numOfEights = 0;
@@ -42,45 +53,35 @@ void main()
 		numOfEights++;
 	}
 
-	int* result = new int[counter * 6];
+	size /= 6;
 
-	for (int i = 0, s = 0; s < counter; i += 3, s += 4)
+	int* result = new int[size * 6];
+	for (int i = 0; i < size * 6; i++)
+		result[i] = 0;
+
+	for (int i = 0, s = 0; s < size; i += 3, s += 4)
 	{
 		result[s] = (arr[i] & 252) >> 2;
 	}
 
-	for (int i = 0 ,j = 1, s = 1; s < counter; i += 3, j += 3, s += 4)
+	for (int i = 0 ,j = 1, s = 1; s < size; i += 3, j += 3, s += 4)
 	{
 		result[s] = ((arr[i] & 3) << 4) + ((arr[j] & 240) >> 4);
 	}
 
-	for (int i = 1, j = 2, s = 2; s < counter; i += 3, j += 3, s += 4)
+	for (int i = 1, j = 2, s = 2; s < size; i += 3, j += 3, s += 4)
 	{
 		result[s] = ((arr[i] & 15) << 2) + ((arr[j] & 192) >> 6);
 	}
 
-	for (int i = 2, s = 3; s < counter; i += 3, s += 4)
+	for (int i = 2, s = 3; s < size; i += 3, s += 4)
 	{
 		result[s] = arr[i] & 63;
 	}
 
-	fileOutput(result, counter, code);
+	fileOutput(result, size, code, numOfEights);
 	//system("pause");
 }
 
 //Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure.
 //AAAA = QUFBQQ==
-
-//cout << "array: ";
-//for (int k = 0; k < counter; k++)
-//{
-//	cout << bitset<8>(arr[k]) << " , ";
-//}
-//cout << "\n";
-//cout << "\n";
-//cout << "return: ";
-//for (int k = 0; k < counter; k++)
-//{
-//	cout << bitset<8>(result[k]) << " , ";
-//}
-//cout << "\n";
