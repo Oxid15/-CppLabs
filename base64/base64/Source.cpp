e483a96a21f3b91e3aadd166faf9c4405488f0e6
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2018-05-11 18:36:09 +0500

    8.2

--- base64/base64/Source.cpp
+++ base64/base64/Source.cpp
@@ -14,9 +14,8 @@ char* getName()
 	return newName;
 }
 
-int* fileInput(int &i)
+int* fileInput(int* arr,int &i)
 {
-	int arr[2048];
 	for (int j = 0; j < 2048; j++)
 		arr[j] = 0;
 
@@ -31,12 +30,7 @@ int* fileInput(int &i)
 	i--;
 	arr[i] = 0;
 	file.close();
-	//int* result = new int[i];
-	//for (int j = 0; j < i; j++)
-	//{
-	//	result[j] = arr[j];
-	//}
-	//return result;
+
 	return arr;
 }
 
@@ -120,15 +114,17 @@ int* decode(int* arr, int size)
 {
 	int* result = new int;
 
-	for (int i = 0, j = 1, s = 0; i < size; i += 4, j += 4, s += 3)
-	{
-		result[s] = (arr[i] << 2) + ((arr[j] & 48) >> 4);
-	}
+	//for (int i = 0, j = 1, s = 0; s < size; i += 4, j += 4, s += 3)
+	//{
+	//	result[s] = (arr[i] << 2) + ((arr[j] & 48) >> 4);
+	//	cout << result[s] << " ";
+	//}
 
-	for (int i = 1, j = 2, s = 1; s < size; i += 4, j += 4, s += 3)
-	{
-		result[s] = ((arr[i] & 15) << 4) + ((arr[j] >> 2) & 15);
-	}
+	//for (int i = 1, j = 2, s = 1; s < size; i += 4, j += 4, s += 3)
+	//{
+	//	result[s] = ((arr[i] & 15) << 4) + ((arr[j] >> 2) & 15);
+	//	cout << result[s] << " ";
+	//}
 
 	//for (int i = 2, j = 3, s = 2; s < size; i += 4, j += 4, s += 3)
 	//{
@@ -144,36 +140,38 @@ void main()
 {
 	char code[] = { "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/" };
 
-	//int size;
+	int size;
 	//int* arr = new int;
-	//arr = fileInput(size);
-	//size *= 8;
-	//int numOfEights = 0;
-	//while (size % 6 != 0)
-	//{
-	//	size += 8;
-	//	numOfEights++;
-	//}
-	//size /= 6;
-
-	//int* result = encode(arr, size);
-	//fileOutputEncode(result, size, code, numOfEights);
-
-	int size_2;
-	int* arr_2 = new int;
-	arr_2 = _fileInput(size_2);
-	int numOfEights_2 = 0;
-	for (int i = size_2; i > (size_2 - 3); i--)
+	int* arr = new int;
+	int newArr[2048];
+	arr = fileInput(newArr,size);
+	size *= 8;
+	int numOfEights = 0;
+	while (size % 6 != 0)
 	{
-		if (arr_2[i] == '=')
-			numOfEights_2++;
+		size += 8;
+		numOfEights++;
 	}
-	size_2 *= 6;
-	size_2 -= numOfEights_2 * 8;
-	size_2 /= 8; 
-	
-	int* result_2 = decode(arr_2, size_2);
-	fileOutputDecode(result_2, size_2);
+	size /= 6;
+
+	int* result = encode(arr, size);
+	fileOutputEncode(result, size, code, numOfEights);
+
+	//int size_2;
+	//int* arr_2 = new int;
+	//arr_2 = _fileInput(size_2);
+	//int numOfEights_2 = 0;
+	//for (int i = size_2; i > (size_2 - 3); i--)
+	//{
+	//	if (arr_2[i]+65 == '=')
+	//		numOfEights_2++;
+	//}
+	//size_2 *= 6;
+	//size_2 -= numOfEights_2 * 8;
+	//size_2 /= 8; 
+	//
+	//int* result_2 = decode(arr_2, size_2);
+	//fileOutputDecode(result_2, size_2);
 }
 
 //Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure.
