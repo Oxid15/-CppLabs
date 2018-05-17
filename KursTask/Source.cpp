78930d739919bdd988a20a3146a0b37e25c2a9a3
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2018-05-17 20:20:53 +0500

    1

--- KursTask/Source.cpp
+++ KursTask/Source.cpp
@@ -1,12 +1,39 @@
+#include <iostream>
+
 using namespace std;
+
+//template<class T>
+//ostream& operator << ((ostream& out, const T& inst));
+//
+//template<class T>
+//ostream& operator >> ((ostream& out, const T& inst));
 
 template <class Tkey, class Tvalue>
 class Collection
 {
-	
+public:
+	Tkey key;
+	Tvalue value;
+
+	friend ostream& operator <<(ostream& out, const Collection& inst)
+	{
+		out << inst.value;
+		return out;
+	}
+
+	friend ostream& operator >>(ostream& in, const Collection& inst)
+	{
+		in >> inst.value;
+		return in;
+	}
 };
+
+
 
 void main()
 {
-
+	int key = 1, value = 0;
+	Collection<int,int> a;
+	a.value = value;
+	cout << a;
 }
