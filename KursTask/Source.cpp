3b3c6ed970856eb4679c87e9751737ae50e4ec91
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2018-05-19 16:33:22 +0500

    4.2

--- KursTask/Source.cpp
+++ KursTask/Source.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-template < class TKey, class TValue > class Collection
+template <class TKey, class TValue> class Collection
 {
 	struct CollectionElement
 	{
@@ -17,8 +17,7 @@ template < class TKey, class TValue > class Collection
 	void arrayExpansion()
 	{
 		int newLength = length * 2;
-		CollectionElement **newArray =
-			new CollectionElement[newLength];
+		CollectionElement **newArray = new CollectionElement[newLength];
 		for (int i = 0; i < length; i++)
 		{
 			newArray[i] = arr[i];
@@ -36,104 +35,71 @@ public:
 		arr = new CollectionElement *[length];
 	}
 
-	friend ostream & operator << (ostream & out, const Collection & inst)
-	{
-		out << inst.value;
-		return out;
-	}
-
-	friend istream & operator >> (istream & in, Collection < TKey,
-		TValue > &inst)
-	{
-		TValue val;
-		in >> val;
-		inst.value = val;
-		return in;
-	}
-
-	//TValue getValue()
+	//friend ostream & operator << (ostream & out, const Collection &inst)
 	//{
+
 	//}
 
-	//TKey getKey()
+	//friend istream & operator >> (istream & in, Collection &inst)
 	//{
-	//}
 
-	//void setValue(TValue val)
-	//{
-	//      value = val;
-	//}
-
-	//void setKey(TKey k)
-	//{
-	//      key = k;
 	//}
 
 	void add(TKey newKey, TValue newValue)
 	{
-
+		
 	}
 
 };
-
-//template <class T>
-//T getT()
+//
+//class Bus
 //{
-//      T smpl;
-//      cout << "Enter the data:\n";
-//      cin >> smpl;
-//      return smpl;
-//}
+//	int num;
+//	char *nameOfDriver;
+//	int routeNum;
+//};
+//
+//class BusPark
+//{
+//	Collection < int, Bus > onRoad;
+//	Collection < int, Bus > inPark;
+//
+//	void fileOut(char *list, int size)
+//	{
+//		ofstream ofile("result.txt");
+//		for (int i = 0; i < size; i++)
+//		{
+//			ofile << list[i];
+//		}
+//		ofile.close();
+//	}
+//
+//public:
+//
+//	BusPark()
+//	{
+//
+//	}
+//
+//	void busOut()
+//	{
+//
+//	}
+//
+//	void busIn()
+//	{
+//
+//	}
+//
+//	char *returnList(Collection < int, Bus > coll, char *buf)
+//	{
+//		int j = 0;
+//
+//	}
+//};
 
-class Bus
+int main()
 {
-	int num;
-	char *nameOfDriver;
-	int routeNum;
-};
-
-class BusPark
-{
-	Collection < int, Bus > onRoad;
-	Collection < int, Bus > inPark;
-
-	void fileOut(char *list, int size)
-	{
-		ofstream ofile("result.txt");
-		for (int i = 0; i < size; i++)
-		{
-			ofile << list[i];
-		}
-		ofile.close();
-	}
-
-public:
-
-	BusPark()
-	{
-
-	}
-
-	void busOut()
-	{
-
-	}
-
-	void busIn()
-	{
-
-	}
-
-	char *returnList(Collection < int, Bus > coll, char *buf)
-	{
-		int j = 0;
-
-	}
-};
-
-int
-main()
-{
-	Collection < int, int >c;
+	Collection < int, int > c;
 	return 0;
 }
