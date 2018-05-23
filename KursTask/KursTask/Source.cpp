ec9dde918cca529924d9f92cc6835b8f447a3325
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2018-05-23 23:50:09 +0500

    6.2

--- KursTask/KursTask/Source.cpp
+++ KursTask/KursTask/Source.cpp
@@ -4,25 +4,19 @@
 using namespace std;
 
 
-template <class TKey, class TValue>
-TKey getKey()
+template <class T>
+T get()
 {
-	TKey key;
-	cin >> key;
-	return key;
-}
-
-template <class TKey, class TValue>
-TValue getValue()
-{
-	TValue value;
-	cin >> value;
-	return value;
+	T val;
+	cout << "Enter the data\n";
+	cin >> val;
+	return val;
 }
 
 template <class TKey, class TValue> 
 class Collection
 {
+private:
 	struct CollectionElement
 	{
 		TKey key;
@@ -44,14 +38,42 @@ class Collection
 		length = newLength;
 	}
 	
-	void setKey(TKey newKey, int num)
+	void setKey(TKey newKey, int idx)
 	{
-		arr[num]->key = newKey;
+		arr[idx]->key = newKey;
 	}
 
-	void setValue(TValue newValue, int num)
+	void setValue(TValue newValue, int idx)
 	{
-		arr[num]->value = newValue;
+		arr[idx]->value = newValue;
+	}
+
+	static bool inputCheck(char* str)								 // ?
+	{
+		bool isCommaFlag = false;
+		bool isSpacesFlag = false;
+
+		for (int i = 0; i < strlen(str); i++)
+		{
+			if (isCommaFlag && isSpacesFlag)
+			{
+				return true;
+			}
+			else
+			{
+				if (str[i] == ',')
+					isCommaFlag = true;
+				if (str[i] == ' ')
+					isSpacesFlag = true;
+			}
+		}
+
+		return false;
+	}
+
+	void decreaseNumOfElem(int number)
+	{
+		numOfElem = numOfElem - number;										   //?
 	}
 
 public:
@@ -81,7 +103,7 @@ public:
 		return numOfElem;
 	}
 		 
-	friend ostream &operator << (ostream & out, Collection inst)
+	friend ostream &operator << (ostream & out, Collection &inst)
 	{
 		for (int i = 0; i < inst.returnNumOfElem(); i++)
 		{
@@ -90,24 +112,71 @@ public:
 		return out;
 	}
 
-	//friend istream &operator >> (istream & in, Collection &inst)
-	//{
+	friend istream &operator >> (istream & in, Collection &inst)
+	{
+		char* strBuf = new char[1024];
+		in.getline(strBuf, 1024);											 // strBuf?
 
-	//}
+		if (inputCheck(strBuf))
+		{
+			int len = strlen(strBuf);
+			int numberOfPairs = 0;
+			for (int i = 0; i < len; i++)
+			{
+				if (strBuf[i] == ',')
+					numberOfPairs++;
+			}
 
-	void add()
+			int keyCnt = 0;
+			TKey* keyArr = new TKey[numberOfPairs];
+			int valCnt = 0;
+			TValue* valArr = new TValue[numberOfPairs];
+
+			for (int i = 0; i < numberOfPairs; i++)
+			{
+				int j = 0;
+				char* buf = new char[256];
+				while (strBuf[j] != ',')
+				{
+					buf[j] = strBuf[j];
+					j++;
+				}
+				//keyArr[keyCnt] = (TKey)buf;								 // ?
+			}
+
+			delete[] strBuf;
+			delete strBuf;
+
+			return in;
+		}
+		else
+			return in;
+	}
+
+	void add(TKey newKey, TValue newValue)
 	{
 		if (numOfElem >= length)
 		{
 			arrayExpansion();
 		}
 
-		TKey newKey = getKey<TKey, TValue>();
-		TValue newValue = getValue<TKey, TValue>();
-
 		setKey(newKey, returnNumOfElem());
 		setValue(newValue, returnNumOfElem());
 		numOfElem++;
+	}
+
+	void del(int num)													
+	{
+		if (num > 0)
+			int idx = num - 1;
+		else
+			return;
+
+		for (int i = idx; i < returnNumOfElem(); i++)
+		{
+			arr[i] = arr[i + 1];
+		}
+		decreaseNumOfElem(1);
 	}
 
 };
@@ -162,12 +231,21 @@ public:
 void main()
 {
 	Collection<int, int> a;
-	a.add();
-	a.add();
+	cout << "Type the key:\n";
+	int newKey = get<int>();
+	cout << "Type the value:\n";
+	int newValue = get<int>(); 
+	a.add(newKey,newValue);
 
+	cout << "Type the key:\n";
+	newKey = get<int>();
+	cout << "Type the value:\n";
+	newValue = get<int>(); 
+	a.add(newKey, newValue);
+
+	//cin >> a;
 	cout << a;
 };
 
-/*
-1,880\n2,228\n3,1547\n4,12\n
-*/
+
+//1,2 3,4 5,6
